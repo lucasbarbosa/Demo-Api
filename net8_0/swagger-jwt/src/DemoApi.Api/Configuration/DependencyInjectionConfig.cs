8e62f78b1654368571a9e75362d0767d14103a91
Lucas Barbosa <lucasbtsilva@gmail.com>
2025-12-25 23:37:55 -0300

    feat: add FluentValidation on model

--- net8_0/swagger-jwt/src/DemoApi.Api/Configuration/DependencyInjectionConfig.cs
+++ net8_0/swagger-jwt/src/DemoApi.Api/Configuration/DependencyInjectionConfig.cs
@@ -1,9 +1,12 @@
 ﻿using DemoApi.Application.Interfaces;
 using DemoApi.Application.Services;
+using DemoApi.Application.Validators.Products;
 using DemoApi.Domain.Handlers;
 using DemoApi.Domain.Interfaces;
 using DemoApi.Infra.CrossCutting.Logging;
 using DemoApi.Infra.Data.Repositories;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using ILogger = DemoApi.Infra.CrossCutting.Interfaces.ILogger;
 
 namespace DemoApi.Api.Configuration
@@ -14,6 +17,14 @@ namespace DemoApi.Api.Configuration
 
         public static IServiceCollection AddDependencyInjectionConfig(this IServiceCollection services)
         {
+            #region FluentValidation
+
+            services.AddFluentValidationAutoValidation();
+            services.AddFluentValidationClientsideAdapters();
+            services.AddValidatorsFromAssemblyContaining<ProductValidator>();
+
+            #endregion
+
             #region Applications
 
             services.AddScoped<IProductAppService, ProductAppService>();
