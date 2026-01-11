918ddd53c750db8b995bb4ce92b6e1e7c6ff9522
Lucas Barbosa <lucasbtsilva@gmail.com>
2026-01-10 21:07:09 -0300

    refactor: rename test projects - swagger sln

---
+++ net8_0/swagger/tests/DemoApi.Api.Tests/Common/Factories/CustomWebApplicationFactory.cs.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace DemoApi.Api.Tests.Common.Factories
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+    }
+}
---
+++ net8_0/swagger/tests/DemoApi.Api.Tests/Products/ProductApiTests.cs
@@ -0,0 +1,36 @@
+using DemoApi.Api.Tests.Common.Factories;
+using DemoApi.Api.Tests.Common.Helpers;
+using DemoApi.Application.Models;
+using DemoApi.Application.Models.Products;
+using DemoApi.Tests.Builders.Products;
+using System.Text.Json;
+
+namespace DemoApi.Api.Tests.Products
+{
+    public class ProductApiTests(CustomWebApplicationFactory factory) : IClassFixture<CustomWebApplicationFactory>
+    {
+        #region Properties
+
+        protected readonly HttpClient _client = factory.CreateClient();
+
+        #endregion
+        
+        #region Protected Methods
+
+
+        protected async Task<ProductViewModel> GetLastCreatedProduct()
+        {
+            string url = "/api/v1/products";
+            ProductViewModel newProduct = ProductViewModelBuilder.New().Build();
+            (HttpResponseMessage _, ResponseViewModel? createResponse) = await HttpClientHelper.PostAndReturnResponseAsync(_client, url, newProduct);
+
+            ProductViewModel? createdProduct = JsonSerializer.Deserialize<ProductViewModel>(
+                createResponse!.Data!.ToString()!,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return createdProduct!;
+        }
+
+        #endregion
+    }
+}
