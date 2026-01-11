9a66cf64a4609c7fe848e08def345960079ed343
Lucas Barbosa <lucasbtsilva@gmail.com>
2026-01-10 21:15:08 -0300

    refactor: rename test projects - docker sln

---
+++ net8_0/swagger-jwt-docker/tests/DemoApi.Application.Tests/Products/GetProductTests.cs
@@ -0,0 +1,137 @@
+using DemoApi.Application.Models.Products;
+using DemoApi.Application.Services;
+using DemoApi.Domain.Entities;
+using DemoApi.Domain.Interfaces;
+using DemoApi.Tests.Builders.Products;
+using FluentAssertions;
+using Moq;
+
+namespace DemoApi.Application.Tests.Products
+{
+    public class GetProductTests : ProductTests
+    {
+        [Fact]
+        public async Task GetAll_ShouldReturnAllProducts_WhenRepositoryHasProducts()
+        {
+            // Arrange
+            (Mock<INotificatorHandler> notificator, Mock<IProductRepository> productRepository, ProductAppService productApplication) = SetProductAppService();
+
+            List<Product> productsFake =
+            [
+                ProductBuilder.New().Build(),
+                ProductBuilder.New().Build(),
+                ProductBuilder.New().Build()
+            ];
+
+            productRepository
+                .Setup(x => x.GetAll())
+                .ReturnsAsync(productsFake);
+
+
+            // Act
+            IList<ProductViewModel> result = await productApplication.GetAll();
+
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().HaveCount(3);
+            result.Should().AllBeOfType<ProductViewModel>();
+
+            productRepository.Verify(
+                x => x.GetAll(),
+                Times.Once
+            );
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnEmptyList_WhenRepositoryHasNoProducts()
+        {
+            // Arrange
+            (Mock<INotificatorHandler> notificator, Mock<IProductRepository> productRepository, ProductAppService productApplication) = SetProductAppService();
+
+            List<Product> productsFake = [];
+
+            productRepository
+                .Setup(x => x.GetAll())
+                .ReturnsAsync(productsFake);
+
+
+            // Act
+            IList<ProductViewModel> result = await productApplication.GetAll();
+
+
+            // Assert
+            result.Should().NotBeNull()
+                .And.BeEmpty();
+
+            productRepository.Verify(
+                x => x.GetAll(),
+                Times.Once
+            );
+        }
+
+        [Fact]
+        public async Task GetById_ShouldReturnProduct_WhenProductExists()
+        {
+            // Arrange
+            (Mock<INotificatorHandler> notificator, Mock<IProductRepository> productRepository, ProductAppService productApplication) = SetProductAppService();
+
+            Product productFake = ProductBuilder.New().Build();
+
+            productRepository
+                .Setup(x => x.GetById(productFake.Id))
+                .ReturnsAsync(productFake);
+
+
+            // Act
+            ProductViewModel? result = await productApplication.GetById(productFake.Id);
+
+
+            // Assert
+            result.Should().NotBeNull();
+            result!.Name.Should().Be(productFake.Name);
+            result.Weight.Should().Be(productFake.Weight);
+
+            productRepository.Verify(
+                x => x.GetById(productFake.Id),
+                Times.Once
+            );
+
+            notificator.Verify(
+                x => x.AddError(It.IsAny<string>()),
+                Times.Never
+            );
+        }
+
+        [Fact]
+        public async Task GetById_ShouldReturnNull_WhenProductDoesNotExist()
+        {
+            // Arrange
+            (Mock<INotificatorHandler> notificator, Mock<IProductRepository> productRepository, ProductAppService productApplication) = SetProductAppService();
+
+            Product nonExistentProduct = ProductBuilder.New().WithId(99999).Build();
+
+            productRepository
+                .Setup(x => x.GetById(nonExistentProduct.Id))
+                .ReturnsAsync((Product?)null);
+
+
+            // Act
+            ProductViewModel? result = await productApplication.GetById(nonExistentProduct.Id);
+
+
+            // Assert
+            result.Should().BeNull();
+
+            productRepository.Verify(
+                x => x.GetById(nonExistentProduct.Id),
+                Times.Once
+            );
+
+            notificator.Verify(
+                x => x.AddError("Product was not found"),
+                Times.Once
+            );
+        }
+    }
+}
---
+++ net8_0/swagger-jwt-docker/tests/DemoApi.Application.Tests/Products/ProductTests.cs
@@ -0,0 +1,52 @@
+using AutoMapper;
+using Bogus;
+using DemoApi.Application.Automapper;
+using DemoApi.Application.Services;
+using DemoApi.Domain.Interfaces;
+using Moq;
+
+namespace DemoApi.Application.Tests.Products
+{
+    public class ProductTests
+    {
+        #region Properties
+
+        protected readonly IMapper _mapper;
+
+        #endregion
+
+        #region Constructors
+
+        public ProductTests()
+        {
+            MapperConfiguration config = new(cfg =>
+            {
+                cfg.AddProfile(new AutomapperConfig());
+            });
+
+            config.AssertConfigurationIsValid();
+            _mapper = config.CreateMapper();
+            
+            Randomizer.Seed = new Random(1234);
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected (Mock<INotificatorHandler>, Mock<IProductRepository>, ProductAppService) SetProductAppService()
+        {
+            Mock<INotificatorHandler> notificator = new();
+            Mock<IProductRepository> productRepository = new();
+            ProductAppService productApplication = new(
+                _mapper,
+                notificator.Object,
+                productRepository.Object
+            );
+
+            return (notificator, productRepository, productApplication);
+        }
+
+        #endregion
+    }
+}
